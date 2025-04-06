1da47585b12a7a57d89e260df9ddad78bbf1fc21
Adonis95 <Sober4202020@outlook.com>
2025-04-06 16:54:11 -0600

    adding load_dotenv()

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 from pathlib import Path
+
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
